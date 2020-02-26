e73f9ed4d6a0a73f0b718d7bbfffe3bb88a79139
Natalie Pina <natalie.a.pina@gmail.com>
2020-02-26 14:44:16 -0600

    initial commit

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,249 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'Hats',
+        routeName: 'hats',
+        items: [
+            {
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: './brown-beanie.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: './blue-beanie.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: './brown-cowboy.png',
+                price: 35
+            },
+            {
+                id: 4,
+                name: 'Grey Brim',
+                imageUrl: './grey-brim.png',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Green Beanie',
+                imageUrl: './green-beanie.png',
+                price: 18
+            },
+            {
+                id: 6,
+                name: 'Palm Tree Cap',
+                imageUrl: './palm-tree-cap.png',
+                price: 14
+            },
+            {
+                id: 7,
+                name: 'Red Beanie',
+                imageUrl: './red-beanie.png',
+                price: 18
+            },
+            {
+                id: 8,
+                name: 'Wolf Cap',
+                imageUrl: './wolf-cap.png',
+                price: 14
+            },
+            {
+                id: 9,
+                name: 'Blue Snapback',
+                imageUrl: './blue-snapback.png',
+                price: 16
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Sneakers',
+        routeName: 'sneakers',
+        items: [
+            {
+                id: 1,
+                name: 'Adidas NMD',
+                imageUrl: './adidas-nmd.png',
+                price: 220
+            },
+            {
+                id: 2,
+                name: 'Adidas Yeezy',
+                imageUrl: './yeezy.png',
+                price: 280
+            },
+            {
+                id: 3,
+                name: 'Black Converse',
+                imageUrl: './black-converse.png',
+                price: 110
+            },
+            {
+                id: 4,
+                name: 'Nike White AirForce',
+                imageUrl: './white-nike-high-tops.png',
+                price: 160
+            },
+            {
+                id: 5,
+                name: 'Nike Red High Tops',
+                imageUrl: './nikes-red.png',
+                price: 160
+            },
+            {
+                id: 6,
+                name: 'Nike Brown High Tops',
+                imageUrl: './nike-brown.png',
+                price: 160
+            },
+            {
+                id: 7,
+                name: 'Air Jordan Limited',
+                imageUrl: './nike-funky.png',
+                price: 190
+            },
+            {
+                id: 8,
+                name: 'Timberlands',
+                imageUrl: './timberlands.png',
+                price: 200
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Jackets',
+        routeName: 'jackets',
+        items: [
+            {
+                id: 1,
+                name: 'Black Jean Shearling',
+                imageUrl: './black-shearling.png',
+                price: 125
+            },
+            {
+                id: 2,
+                name: 'Blue Jean Jacket',
+                imageUrl: './blue-jean-jacket.png',
+                price: 90
+            },
+            {
+                id: 3,
+                name: 'Grey Jean Jacket',
+                imageUrl: './grey-jean-jacket.png',
+                price: 90
+            },
+            {
+                id: 4,
+                name: 'Brown Shearling',
+                imageUrl: './brown-shearling.png',
+                price: 165
+            },
+            {
+                id: 5,
+                name: 'Tan Trench',
+                imageUrl: './brown-trench.png',
+                price: 185
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: 'Womens',
+        routeName: 'womens',
+        items: [
+            {
+                id: 1,
+                name: 'Blue Tanktop',
+                imageUrl: './blue-tank.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Floral Blouse',
+                imageUrl: './floral-blouse.png',
+                price: 20
+            },
+            {
+                id: 3,
+                name: 'Floral Dress',
+                imageUrl: './floral-skirt.png',
+                price: 80
+            },
+            {
+                id: 4,
+                name: 'Red Dots Dress',
+                imageUrl: './red-polka-dot-dress.png',
+                price: 80
+            },
+            {
+                id: 5,
+                name: 'Striped Sweater',
+                imageUrl: './striped-sweater.png',
+                price: 45
+            },
+            {
+                id: 6,
+                name: 'Yellow Track Suit',
+                imageUrl: './yellow-track-suit.png',
+                price: 135
+            },
+            {
+                id: 7,
+                name: 'White Blouse',
+                imageUrl: './white-vest.png',
+                price: 20
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: 'Mens',
+        routeName: 'mens',
+        items: [
+            {
+                id: 1,
+                name: 'Camo Down Vest',
+                imageUrl: './camo-vest.png',
+                price: 325
+            },
+            {
+                id: 2,
+                name: 'Floral T-shirt',
+                imageUrl: './floral-shirt.png',
+                price: 20
+            },
+            {
+                id: 3,
+                name: 'Black & White Longsleeve',
+                imageUrl: './long-sleeve.png',
+                price: 25
+            },
+            {
+                id: 4,
+                name: 'Pink T-shirt',
+                imageUrl: './pink-shirt.png',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Jean Long Sleeve',
+                imageUrl: './roll-up-jean-shirt.png',
+                price: 40
+            },
+            {
+                id: 6,
+                name: 'Burgundy T-shirt',
+                imageUrl: './polka-dot-shirt.png',
+                price: 25
+            }
+        ]
+    }
+];
+
+export default SHOP_DATA;
