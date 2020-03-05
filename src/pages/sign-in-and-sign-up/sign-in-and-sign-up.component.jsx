003fcb18d934b821a658590476408ba0c75220d1
Natalie Pina <natalie.a.pina@gmail.com>
2020-03-05 15:39:34 -0600

    remove unnecessary braces

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -5,7 +5,7 @@ import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
-  <div className={'sign-in-and-sign-up'}>
+  <div className='sign-in-and-sign-up'>
       <SignIn />
       <SignUp />
   </div>
