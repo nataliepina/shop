dc1fa317745062f40bc9077b5f700b34bdf0a279
Natalie Pina <natalie.a.pina@gmail.com>
2020-03-06 10:42:18 -0600

    integrate reselect and createStructuredSelector

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,14 @@ import './header.style.scss';
 
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors'
 
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 
@@ -37,10 +40,10 @@ const Header = ({ currentUser, hidden }) => (
       {hidden ? null : <CartDropdown />}
     </div>
   );
-  
-  const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-    currentUser,
-    hidden
+
+  const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
   });
   
   export default connect(mapStateToProps)(Header);
