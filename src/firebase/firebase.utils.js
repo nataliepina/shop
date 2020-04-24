4532211f4852cc4c2d96183af8ffa84462899804
Natalie Pina <natalie.a.pina@gmail.com>
2020-04-24 12:49:05 -0500

    firebase updates

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -41,6 +41,18 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch()
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  await batch.commit()
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
