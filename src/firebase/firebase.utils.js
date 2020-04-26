6393a4b5596936fb612d3331a09575c8d74ccf3c
Natalie Pina <natalie.a.pina@gmail.com>
2020-04-26 18:50:02 -0500

    map over collections

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -51,7 +51,25 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   });
 
   await batch.commit()
-}
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection  = collections.doc.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
