8f50310b9ae46d6dba94a08d10e4d9ea2fa05af3
Natalie Pina <natalie.a.pina@gmail.com>
2020-05-20 17:27:16 -0500

    update for dev vs prod

--- server.js
+++ server.js
@@ -19,18 +19,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
-
+ 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
-
+ 
 app.listen(port, error => {
-  if(error) throw error;
-  console.log('Server running on port ' + port);
-})
+  if (error) throw error;
+  console.log('Server is running on port ' + port);
+});
 
 app.get('/service-worker.js', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
